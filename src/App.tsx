930b239a44c3ecd2a179a87b44f48f075406c60e
Hope Fourie <hopefourie@gmail.com>
2025-08-04 18:09:32 -0400

    paralax working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
-import logo from "./assets/slug-logo.png"
+import logo from "./assets/slug-logo.png";
 
 function App() {
   return (
-    <div className='container'>
-          <div className="circle"/>
+    <div className="container">
+      <div className="logo-container">
+      <div className="circle" />
 
-     <img className="logo" src={logo} />
-      
-      {/* <div className="phone">(555)867-5309</div> */}
+      <img className="logo" src={logo} />
+      </div>
+      <div className="spacer"/>
+      <div className="phone">(804) 396-4723</div>
+      <div className="bottom-spacer"/>
     </div>
   );
 }
